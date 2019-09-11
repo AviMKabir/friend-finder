474f1ffba299ece7f64563e0c2ec9e73820997e2
Avi M Kabir <avimishkakabir@gmail.com>
2019-09-11 15:23:03 -0400

    added more routing and css file need to do some logic still

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [ {
+    "name" : "Bobby Cesaro",
+    "photo" : "https://image.shutterstock.com/image-illustration/male-default-placeholder-avatar-profile-260nw-582509560.jpg",
+    "scores" : [1,2,3,4,5,1,2,3,4,5]
+}, {
+    "name" : "Lance Roman",
+    "photo" : "https://image.shutterstock.com/image-illustration/male-default-placeholder-avatar-profile-260nw-582509560.jpg",
+    "scores" : [1,2,3,4,5,1,2,3,4,4]
+}, {
+    "name" : "Daniel Lugo",
+    "photo" : "https://image.shutterstock.com/image-illustration/male-default-placeholder-avatar-profile-260nw-582509560.jpg",
+    "scores" : [1,2,3,4,5,1,2,3,4,3]
+}, {
+    "name" : "Ryan Baum",
+    "photo" : "https://image.shutterstock.com/image-illustration/male-default-placeholder-avatar-profile-260nw-582509560.jpg",
+    "scores" : [1,2,3,4,5,1,2,3,4,2]
+}, {
+    "name" : "Charlie Catapano",
+    "photo" : "https://image.shutterstock.com/image-illustration/male-default-placeholder-avatar-profile-260nw-582509560.jpg",
+    "scores" : [1,2,3,4,5,1,2,3,4,1]
+}];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -20,11 +20,16 @@ var PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(__dirname + '/app/public'));
+
+
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutres")(app);
